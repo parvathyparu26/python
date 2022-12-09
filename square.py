ecbf25031bbddfeee83b6b30032672243d2a0fd1
parvathyparu26 <117814552+parvathyparu26@users.noreply.github.com>
2022-12-09 06:32:38 -1200

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,8 @@
-nums=[5,30,1,0,-4,-26,-49,6]
-print("original numbers in the list")
-new_nums=list(filter(lambda x: x>0,nums))
-print("positive numbers in the list:",new_nums)
+n=4
+square=n*n
+print(square)
+OUTPUT
+square of 4 is 16
 
 
 
