e98001fa6d067cdb48d08ce0508be1c5d3b8148e
parvathyparu26 <117814552+parvathyparu26@users.noreply.github.com>
2022-12-14 10:19:55 +0530

    Experiment number 7 cycle 2

---
+++ count using strring.py
@@ -0,0 +1,13 @@
+def char_frequency(str1):
+    dict = {}
+    for n in str1:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+print(char_frequency('google.com'))
+
+output
+{'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}
